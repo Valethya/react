66bb1066e7e3d407e339f86c1e2aafe8b3a3f60d
Valentina Rojas <valenthinarojasr@gmail.com>
2022-11-02 11:25:20 -0300

    responsivo en Medium and up

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import CartWidget from "./CartWidget";
 import Badge from "../Badge/Badge";
+import { Menu } from "@material-ui/icons";
 
 const pages = ["Products", "Pricing", "Blog"];
 
 function NavBar(props) {
   return (
     <nav>
+      <Menu className="hamburger-menu" />
       <h1 className="logo">Omori Store</h1>
       <ul>
         {pages.map((page) => (
